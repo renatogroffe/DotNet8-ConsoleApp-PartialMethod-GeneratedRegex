9103c937fd74d4581e61fea883d69b15fe85e006
Renato Groffe <renato.groffe@yahoo.com.br>
2024-09-16 08:51:26 -0300

    .NET 8 + Console Application + Partial Method + atributo GeneratedRegex

---
+++ src/ConsoleAppPartialMethod/Helpers/ValidationHelpers.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleAppPartialMethod.Helpers;
+
+public static partial class ValidationHelpers
+{
+    [GeneratedRegex("sqlserver|postgres|mysql")]
+    public static partial Regex IsRelationalDatabase();
+}
---
+++ src/ConsoleAppPartialMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleAppPartialMethod.Helpers;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com .NET 8 + GeneratedRegexAttribute + Partial Method *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+Console.WriteLine();
+
+var selectedDatabases = new string[] { "sqlserver", "mongodb", "postgres", "redis", "mysql" };
+foreach (var opcao in selectedDatabases)
+{
+    var oldColor = Console.ForegroundColor;
+    if (ValidationHelpers.IsRelationalDatabase().IsMatch(opcao))
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine($"{opcao} = alternativa valida");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{opcao} = alternativa invalida");
+    }
+    Console.ForegroundColor = oldColor;
+}
